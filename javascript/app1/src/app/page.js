9b765d10d91305721cdc288b299c795668d15d6e
Enzo Dante <85023505+enzoDante@users.noreply.github.com>
2024-10-23 11:34:29 -0300

    Update page.js

--- javascript/app1/src/app/page.js
+++ javascript/app1/src/app/page.js
@@ -17,6 +17,21 @@ export default function Home() {
 					descricao={"Projeto que permite adicionar pequenos blocos móveis, onde cada bloco você pode digitar um título e adicionar/remover/check tarefas."} 
 					url={'/ToDoList'}
 				/>
+				<ExpandirRetrair
+					titulo={"Aprovar ou Rejeitar"}
+					descricao={"Projeto onde vários perfis aleatórios irão aparecer no centro da tela, você pode ler cada informação e mover o perfil para esquerda ou direita indicando que aprovou o perfil ou rejeitou ele, sempre que mover um perfil, um novo irá aparecer no centro da tela. Também mostrará quantos foram aprovados e rejeitados."} 
+					url={'/PerfisAprovadoRejeitado'}
+				/>
+				<ExpandirRetrair
+					titulo={"Mover blocos"}
+					descricao={"Projeto que permite contém vários blocos lado a lado e você pode trocá-los de lugar, os blocos são fixos em um determinado local, porém podem mudar de posição com outros blocos."} 
+					url={'/MoverBlocos'}
+				/>
+				<ExpandirRetrair
+					titulo={"Escudo móvel"}
+					descricao={"Você deve manter o cursor sobre o escudo enquanto ele se move, a cada 10 segundos o escudo pode aumentar a velocidade ou diminuir (existe velocidade máxima e tamanho mínimo att)."} 
+					url={'/Proteja-se'}
+				/>
 
 			</article>
 		</main>
